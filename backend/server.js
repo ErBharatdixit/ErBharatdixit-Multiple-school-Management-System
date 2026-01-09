cf1f9ca5ef74785621245daeb750b75d3202c89d
bharat Dixit <bharat010703@gmail.com>
2026-01-09 20:39:35 +0530

    fix our frontend url

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use(
       cors({
-            origin: ["http://localhost:5173", "http://localhost:5174", process.env.FRONTEND_URL].filter(Boolean),
+            origin: ["http://localhost:5173", "http://localhost:5174", "https://erbharatdixit-multiple-school-management.onrender.com", process.env.FRONTEND_URL].filter(Boolean),
             credentials: true,
       })
 );
